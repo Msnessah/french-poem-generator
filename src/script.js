82f97c60bf5ba85d6435165e508e6211d1d5dacc
Msnessah <tshegofatsomashabathakga2710@gmail.com>
2025-08-28 02:24:39 +0200

    names

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ function generateName(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "97eod64d4af071fc4fb21ct3fb439676"; // Replace with your actual API key
+  let apiKey = "97eod64d4af071fc4fb21ct3fb439676"; 
   let context ="You are an incredible name expert. Your mission is to generate a 6 names in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the names. Do not include any additional commentary. Do not apologize. Do not self-reference. Do not explain what you are doing. Just give me the names.";
   let prompt = `User instructions: Generate AI names ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;

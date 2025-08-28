ec1a6d5f0c98fb7cfeb77e60dd2a1fc42c6f504c
Msnessah <tshegofatsomashabathakga2710@gmail.com>
2025-08-28 02:45:47 +0200

    js

--- src/script.js
+++ src/script.js
@@ -13,13 +13,13 @@ function generateName(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "97eod64d4af071fc4fb21ct3fb439676"; 
   let context ="You are an incredible name expert. Your mission is to generate a 6 names in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the names. Do not include any additional commentary. Do not apologize. Do not self-reference. Do not explain what you are doing. Just give me the names.";
-  let prompt = `User instructions: Generate AI names ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate AI Name ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
-  let poemElement = document.querySelector("#name");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating name ${instructionsInput.value}</div>`;
+  let nameElement = document.querySelector("#name");
+  nameElement.classList.remove("hidden");
+  nameElement.innerHTML = `<div class="generating">⏳ Generating name ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayName);
 }
